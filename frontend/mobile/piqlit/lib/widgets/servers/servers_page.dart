13edc7a3c99e2ed084ad627e880a841e2a9a0c9a
brian <bdostilio@sparkfly.com>
2019-08-25 14:44:19 -0400

    added working scrollable list of servers

--- frontend/mobile/piqlit/lib/widgets/servers/servers_page.dart
+++ frontend/mobile/piqlit/lib/widgets/servers/servers_page.dart
@@ -1,38 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../models/server.dart';
-import '../../models/media.dart';
 
 class ServersPage extends StatelessWidget {
   final servers = [
-    Server('192.168.1.2', List<Media>()),
-    Server('192.168.1.3', [
-      Media('backdoor sluts 9', 'eric cartman', DateTime.now()),
-      Media('Jurassic Park', 'Michael Crichton', DateTime.now()),
-      
-    ]),
-    Server('192.168.1.2', List<Media>()),
-    Server('192.168.1.3', [
-      Media('backdoor sluts 9', 'eric cartman', DateTime.now()),
-      Media('Jurassic Park', 'Michael Crichton', DateTime.now()),
-      
-    ]),
-    Server('192.168.1.2', List<Media>()),
-    Server('192.168.1.3', [
-      Media('backdoor sluts 9', 'eric cartman', DateTime.now()),
-      Media('Jurassic Park', 'Michael Crichton', DateTime.now()),
-      
-    ]),
-    Server('192.168.1.2', List<Media>()),
-    Server('192.168.1.3', [
-      Media('backdoor sluts 9', 'eric cartman', DateTime.now()),
-      Media('Jurassic Park', 'Michael Crichton', DateTime.now()),
-    ]),
-    Server('192.168.1.2', List<Media>()),
-    Server('192.168.1.3', [
-      Media('backdoor sluts 9', 'eric cartman', DateTime.now()),
-      Media('Jurassic Park', 'Michael Crichton', DateTime.now()),
-    ]),
+    Server('192.168.1.2', 'Downstairs TV', [], true, true),
+    Server('192.168.1.22', 'Repo', [], false, true),
+    Server('192.168.1.4', 'iMac', [], true, true),
+    Server('192.168.1.8', 'Brian\'s iPad', [], true, true),
+    Server('192.168.1.15', 'Tia\'s iPad', [], true, true),
+    Server('192.168.1.15', 'Top', [], true, false),
   ];
 
   void addServer() {
@@ -41,44 +18,64 @@ class ServersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'Servers',
-              textScaleFactor: 2,
-              textAlign: TextAlign.start,
-              style: TextStyle(fontWeight: FontWeight.w500),
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            // color: Colors.grey,
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Servers',
+                  textScaleFactor: 2,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                RaisedButton(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () => addServer,
+                ),
+              ],
             ),
-            Text('Hello World!')
-          ],
-        ),
-        Container(
-          decoration: BoxDecoration(color: Colors.amber),
-          child: SingleChildScrollView(
-            controller: ScrollController(),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.grey),
             child: Column(
               children: servers.map((Server s) {
                 return Container(
-                  child: Text(
-                    'This is a card',
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.w500,
+                  color: s.reachable ? Colors.green : Colors.red,
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              s.name,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text('IP Address: ${s.ipAddr}'),
+                          ],
+                        ),
+                        Icon( s.reachable ? Icons.check : Icons.clear),
+                      ],
                     ),
                   ),
                 );
               }).toList(),
             ),
           ),
-        ),
-        // ListView(
-
-        // ),
-      ],
+        ],
+      ),
     );
   }
 }
