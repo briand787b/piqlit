dc125bcd6530de501ada97393f5ec00f607f25a7
brian dostilio <briand787b@gmail.com>
2019-09-24 20:22:55 -0400

    wrote tests for decompressor

--- backend/core/obj/decompressor_test.go
+++ backend/core/obj/decompressor_test.go
@@ -1,1 +1,58 @@
 package obj_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/briand787b/piqlit/core/obj"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDecompressor(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name          string
+		file          string
+		expErrToBeNil bool
+	}{
+		{"successfully_decompresses_jpg_pic", "a.jpg", true},
+		{"successfully_decompresses_gif_video", "a.gif", true},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			cF, err := os.Open(filepath.Join("./testdata", t.Name(), "compress.gz"))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			dc, err := obj.NewDecompressor(cF)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer dc.Close()
+
+			dcBS, err := ioutil.ReadAll(dc)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if *update {
+				// write code here
+			}
+
+			expBS, err := ioutil.ReadFile(filepath.Join("testdata", t.Name(), tt.file))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !cmp.Equal(expBS, dcBS) {
+				t.Fatal("expected decompressed data and actual decompressed data are different")
+			}
+		})
+	}
+}
