4597a266baf4c8c2cd26bbf80ef6e70f3c204d9f
brian <bdostilio@sparkfly.com>
2019-09-29 21:11:52 -0400

    Media delete and insert test passes, needs more test cases

--- backend/core/postgres/media_pg_store_test.go
+++ backend/core/postgres/media_pg_store_test.go
@@ -3,11 +3,13 @@ package postgres_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/briand787b/piqlit/core/model"
 	"github.com/briand787b/piqlit/core/obj"
 	"github.com/briand787b/piqlit/core/postgres"
 	"github.com/briand787b/piqlit/core/postgres/postgrestest"
+
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -40,6 +42,20 @@ func TestMediaPGStoreGetByID(t *testing.T) {
 				Encoding:     obj.GIF,
 				UploadStatus: obj.UploadInProgress,
 			},
+		},
+		{
+			"finding_invalid_id_in_single_fails",
+			1,
+			false,
+			false,
+			model.Media{},
+		},
+		{
+			"finding_invalid_id_in_multi_fails",
+			3,
+			false,
+			false,
+			model.Media{},
 		},
 	}
 
@@ -77,6 +93,56 @@ func TestMediaPGStoreGetByID(t *testing.T) {
 				t.Fatalf("expected Media and returned media are not equal: %s",
 					cmp.Diff(tt.expM, *retM),
 				)
+			}
+		})
+	}
+}
+
+func TestMediaPGStoreInsertDelete(t *testing.T) {
+	tests := []struct {
+		name string
+		m    model.Media
+	}{
+		{
+			"successful_with_all_fields_filled_in",
+			model.Media{
+				Name:         "mango",
+				Encoding:     obj.GIF,
+				UploadStatus: obj.UploadInProgress,
+				CreatedAt:    time.Now().UTC().Truncate(time.Second),
+				UpdatedAt:    time.Now().UTC().Truncate(time.Second),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := postgrestest.NewPGHelper(t)
+			mps := postgres.NewMediaPGStore(h.L, h.DB)
+			ctx := context.Background()
+
+			if err := mps.Insert(ctx, &tt.m); err != nil {
+				t.Fatal(err)
+			}
+
+			retM, err := mps.GetByID(ctx, tt.m.ID)
+			if err != nil {
+				t.Fatal("could not get by id: ", err)
+			}
+
+			tt.m.ID = retM.ID
+			if !cmp.Equal(tt.m, *retM) {
+				t.Fatalf("expected Media not equal to returned media %s",
+					cmp.Diff(tt.m, *retM),
+				)
+			}
+
+			if err := mps.DeleteByID(ctx, retM.ID); err != nil {
+				t.Fatal(err)
+			}
+
+			if _, err := mps.GetByID(ctx, tt.m.ID); err == nil {
+				t.Fatal("expected error to be non-nil, was nil")
 			}
 		})
 	}
