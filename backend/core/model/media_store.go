3ca3e31d9181fbf844c9a7bca305989b8f775574
brian <bdostilio@sparkfly.com>
2019-10-03 08:44:35 -0400

    media_store reflects renamed media_pg_store_method

--- backend/core/model/media_store.go
+++ backend/core/model/media_store.go
@@ -2,13 +2,12 @@ package model
 
 import "context"
 
-// MediaStore is anything that can store and retrieve Media records
-// from a database
+// MediaStore is anything that can store and retrieve Media records from a database
 type MediaStore interface {
 	AssociateParentIDWithChildIDs(ctx context.Context, pID int, cIDs ...int) error
 	DeleteByID(ctx context.Context, id int) error
 	DisassociateParentIDFromChildIDs(ctx context.Context, pID int, cIDs ...int) error
-	FindByParentID(ctx context.Context, pID int) ([]Media, error)
 	GetByID(ctx context.Context, id int) (*Media, error)
 	Insert(ctx context.Context, m *Media) error
+	SelectByParentID(ctx context.Context, pID int) ([]Media, error)
 }
--- backend/core/postgres/media_pg_store.go
+++ backend/core/postgres/media_pg_store.go
@@ -101,29 +101,6 @@ func (mps *MediaPGStore) DisassociateParentIDFromChildIDs(ctx context.Context, p
 	return nil
 }
 
-// SelectByParentID returns a slice of model.Media with the provided parentID
-func (mps *MediaPGStore) SelectByParentID(ctx context.Context, pID int) ([]model.Media, error) {
-	var m []model.Media
-	if err := sqlx.SelectContext(ctx, mps.db, &m, `
-		SELECT
-			m.id,
-			m.name,
-			m.encoding,
-			m.upload_status,
-			m.created_at,
-			m.updated_at
-		FROM media m
-		INNER JOIN parent_child_media pcm
-		ON m.id = pcm.child_id
-		WHERE pcm.parent_id = $1;`,
-		pID,
-	); err != nil {
-		return nil, errors.Wrap(err, "could not execute query")
-	}
-
-	return m, nil
-}
-
 // GetByID returns a Media record by its id
 func (mps *MediaPGStore) GetByID(ctx context.Context, id int) (*model.Media, error) {
 	var m model.Media
@@ -180,4 +157,27 @@ func (mps *MediaPGStore) Insert(ctx context.Context, m *model.Media) error {
 
 	m.ID = saveID
 	return nil
+}
+
+// SelectByParentID returns a slice of model.Media with the provided parentID
+func (mps *MediaPGStore) SelectByParentID(ctx context.Context, pID int) ([]model.Media, error) {
+	var m []model.Media
+	if err := sqlx.SelectContext(ctx, mps.db, &m, `
+		SELECT
+			m.id,
+			m.name,
+			m.encoding,
+			m.upload_status,
+			m.created_at,
+			m.updated_at
+		FROM media m
+		INNER JOIN parent_child_media pcm
+		ON m.id = pcm.child_id
+		WHERE pcm.parent_id = $1;`,
+		pID,
+	); err != nil {
+		return nil, errors.Wrap(err, "could not execute query")
+	}
+
+	return m, nil
 }
