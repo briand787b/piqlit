9fdd2f129b70140683b73b6c85a9d36d88b263ba
brian <bdostilio@sparkfly.com>
2019-10-03 08:34:58 -0400

    added parent_child operations to media_store

--- backend/core/model/media_store.go
+++ backend/core/model/media_store.go
@@ -5,7 +5,9 @@ import "context"
 // MediaStore is anything that can store and retrieve Media records
 // from a database
 type MediaStore interface {
+	AssociateParentIDWithChildIDs(ctx context.Context, pID int, cIDs ...int) error
 	DeleteByID(ctx context.Context, id int) error
+	DisassociateParentIDFromChildIDs(ctx context.Context, pID int, cIDs ...int) error
 	FindByParentID(ctx context.Context, pID int) ([]Media, error)
 	GetByID(ctx context.Context, id int) (*Media, error)
 	Insert(ctx context.Context, m *Media) error
