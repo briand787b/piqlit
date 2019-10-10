4b754c79a92fb316afef35fc48e2af0637056414
brian <bdostilio@sparkfly.com>
2019-10-10 09:47:21 -0400

    added unique index on media.name table

--- backend/core/postgres/init-db.sql
+++ backend/core/postgres/init-db.sql
@@ -4,13 +4,16 @@ DROP TABLE IF EXISTS media;
 
 CREATE TABLE media (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
     length bigint NOT NULL,
     encoding VARCHAR(55) NOT NULL,
     upload_status VARCHAR(55) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE UNIQUE INDEX media_name 
+ON media (name);
 
 CREATE TABLE parent_child_media (
     parent_id INTEGER REFERENCES media(id) NOT NULL,
