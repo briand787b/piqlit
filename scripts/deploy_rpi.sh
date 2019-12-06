6534981b0fc40a4e5828f3b408f508f91b587806
brian dostilio <briand787b@gmail.com>
2019-12-06 18:23:18 -0500

    added docker stack and deployment script

---
+++ scripts/deploy_rpi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git pull
+docker login -u briand787b -p $DOCKER_HUB_PASSWORD
+
+# build frontend-web-vue
+docker image build -t briand787b/piqlit-vue-frontend:$(git rev-parse HEAD)-arm ./frontend/web/vue
+docker image push briand787b/piqlit-vue-frontend:$(git rev-parse HEAD)-arm
+docker image tag briand787b/piqlit-vue-frontend:$(git rev-parse HEAD)-arm briand787b/piqlit-vue-frontend:deployed-arm
+docker image push briand787b/piqlit-vue-frontend:deployed-arm
+
+# build postgresql
+docker image build -t briand787b/piqlit-pg-db:$(git rev-parse HEAD)-arm ./backend/core/postgres
+docker image push briand787b/piqlit-pg-db:$(git rev-parse HEAD)-arm
+docker image tag briand787b/piqlit-pg-db:$(git rev-parse HEAD)-arm briand787b/piqlit-pg-db:deployed-arm
+docker image push briand787b/piqlit-pg-db:deployed-arm
+
+# build backend-go
+docker image build \
+    -t briand787b/piqlit-go-backend:$(git rev-parse HEAD)-arm \
+    --build-arg arch=arm64 \
+    -f ./backend/api/rest/Dockerfile \
+    ./backend
+docker image push briand787b/piqlit-go-backend:$(git rev-parse HEAD)-arm
+docker image tag briand787b/piqlit-go-backend:$(git rev-parse HEAD)-arm briand787b/piqlit-go-backend:deployed-arm
+docker image push briand787b/piqlit-go-backend:deployed-arm
