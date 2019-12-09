34c9663a6164399ddd7e5cb59aa7c6bf05bac415
brian dostilio <briand787b@gmail.com>
2019-12-09 18:34:02 -0500

    added commented out config for do

--- infrastructure/modules/inlets/digital_ocean.tf
+++ infrastructure/modules/inlets/digital_ocean.tf
@@ -4,3 +4,11 @@ provider digitalocean {
 }
 
 data "digitalocean_account" "account" {}
+
+# # Create a new Web Droplet in the nyc2 region
+# resource "digitalocean_droplet" "inlet" {
+#   image  = "ubuntu-18-04-x64"
+#   name   = "inlet"
+#   region = "nyc2"
+#   size   = "s-1vcpu-1gb"
+# }
